7ff74d1302f42d036a23dc0672b919b45ff203ad
Arunesh Mishra <aruneshm@gmail.com>
2023-02-05 19:14:10 -0700

    add buffering

--- scripts/content.js
+++ scripts/content.js
@@ -6,14 +6,36 @@ const satlng = document.querySelector("#satlng");
 
 console.log("From content script");
 
+var dataBuffer = [];
+var jqxhrPromise = undefined;
+var inBufferMode = false;
+
+// Keeps buffering until the previous POST finishes (success or fail).
 function sendData(lat, lng) {
-    $.post("http://localhost:9090/issloc",
-        {lat:lat, lng: lng}, function (response) {
-            if (response) console.log(response);
+
+    dataBuffer.push([lat, lng, Date.now()]);
+
+    if (!inBufferMode) {
+        var dataCopy = dataBuffer;
+        dataBuffer = [];
+        console.log("Sending to server: " + dataCopy.length);
+        jqxhrPromise = $.post("http://localhost:9090/issloc",
+            {data:JSON.stringify(dataCopy)}, function (response) {
+                if (response) console.log(response);
+            });
+
+        jqxhrPromise.always(() => {
+            // Turn off buffering.
+            inBufferMode = false;
+            console.log("Buffering turned off at len = " + dataBuffer.length);
         });
+    } else {
+        console.log("In buffering mode for lat/lng =" + lat + ", " + lng);
+    }
+
 }
 
-const readingTimer = setInterval(()=> {
+const readingTimer = setInterval(() => {
     console.log(satlat.innerHTML + " , " + satlng.innerHTML);
     sendData(satlat.innerHTML, satlng.innerHTML);
 }, 1000);
--- server/keyvector-server.js
+++ server/keyvector-server.js
@@ -11,6 +11,16 @@ app.use(express.urlencoded({ extended: true }))
 
 app.post('/issloc', (req, resp) => {
     console.log("Received values:" + JSON.stringify(req.body));
+    if (req.body.data) {
+        csvList = JSON.parse(req.body.data);
+        csvString = ""
+        csvList.forEach(element => {
+            csvString += `${element[0]}, ${element[1]}, ${element[2]}\n`;
+        });
+        console.log("csvString =" + csvString);
+        fs.appendFileSync("iss-location.csv", csvString);
+        resp.send({ Success: `Received lat = ${lat}, lng = ${lng}`, redirect: true});
+    } else 
     if (req.body.lat && req.body.lng) {
         var lat = req.body.lat;
         var lng = req.body.lng;
